fc9663939d9c6e855dd0b041fe388d969ac1c2b1
Shawn Huang <huanghsieh@gmail.com>
2025-07-30 00:24:55 +0800

    work on searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Chart from "@/components/Chart";
 import DataTable from "@/components/DataTable";
 import Header from "@/components/Header";
@@ -5,18 +7,19 @@ import SearchBar from "@/components/SearchBar";
 import styles from "@/css/App.module.css";
 
 export default function App() {
-    return (
-        <>
-            <SearchBar />
-            <div className={styles.pos_header}>
-                <Header />
-            </div>
-            <div className={styles.pos_chart}>
-                <Chart />
-            </div>
-            <div className={styles.pos_datatable}>
-                <DataTable />
-            </div>
-        </>
-    );
+
+  return (
+    <>
+      <SearchBar />
+      <div className={styles.pos_header}>
+        <Header />
+      </div>
+      <div className={styles.pos_chart}>
+        <Chart />
+      </div>
+      <div className={styles.pos_datatable}>
+        <DataTable />
+      </div>
+    </>
+  );
 }
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,50 +1,104 @@
 "use client";
 
-import { Box, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
-import { useContext, useState } from "react";
-import { DataContext } from "./AppProvider";
+import {
+  Box,
+  Divider,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Stack,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useContext, useRef, useState } from "react";
+import { DataContext, StockInfoType } from "./AppProvider";
 
-export default function SearchBar () {
-    const { stockList } = useContext(DataContext);
-    const {data} = stockList;
-    const [inputValue, setInputValue] = useState("");
-    
-    console.log("stockList: ", JSON.stringify(data));
+export default function SearchBar() {
+  const { stockList } = useContext(DataContext);
+  const { data } = stockList;
+  const searchable: string[] = [
+    ...new Set(
+      (data as StockInfoType[]).map((o) => `${o.stock_id}-${o.stock_name}`)
+    ),
+  ];
+  const [inputValue, setInputValue] = useState("");
+  const [searchResults, setSearchResults] = useState<string[]>([]);
+  const inputRef = useRef(null);
 
-    function handleChange (e: React.ChangeEvent<HTMLInputElement>) {
-        const value = e.currentTarget.value;
-        setInputValue(value);
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.currentTarget.value;
+    setInputValue(value);
+    handleSearch(value);
+  }
+
+  function handleKeydown(e: React.KeyboardEvent<HTMLInputElement>) {
+    const key = e.key;
+    if (key === "Enter") handleSearch(inputValue);
+  }
+
+  function handleSearch(value: string) {
+    if (!!value) {
+      const results = searchable.filter((str) => str.includes(value));
+      setSearchResults(results);
+    } else {
+      setSearchResults([]);
     }
-
-    function handleKeydown (e: React.KeyboardEvent<HTMLInputElement>) {
-        const key = e.key;
-        if (key === "Enter") handleSearch(inputValue);
-    }
-
-    function handleSearch (value: string) {
-        console.log("Searching: ", value);
-    }
-    return (
-        <Box sx={{backgroundColor: "#FFFFFF", width: "100%", height: "60px", display: "flex", justifyContent: "center", alignItems: "center", position: "absolute", top: 0, left: 0}}>
-            <FormControl variant="outlined" size="small">
-                <InputLabel htmlFor="outlined-adornment-password">輸入台 / 美股代號，查看公司價值</InputLabel>
-                <OutlinedInput
-                    value={inputValue}
-                    onChange={handleChange}
-                    onKeyDown={handleKeydown}
-                    id="outlined-adornment-password"
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton onClick={() => handleSearch(inputValue)}>
-                                <SearchIcon />
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                    label="輸入台 / 美股代號，查看公司價值"
-                    sx={{width: "320px"}}
-                />
-            </FormControl>
-        </Box>
-    );
+  }
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#FFFFFF",
+        width: "100%",
+        height: "60px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        zIndex: "1",
+      }}
+    >
+      <FormControl variant="outlined" size="small">
+        <InputLabel htmlFor="outlined-adornment-password">
+          輸入台 / 美股代號，查看公司價值
+        </InputLabel>
+        <OutlinedInput
+          ref={inputRef}
+          value={inputValue}
+          onChange={handleChange}
+          onKeyDown={handleKeydown}
+          id="outlined-adornment-password"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton onClick={() => handleSearch(inputValue)}>
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          }
+          label="輸入台 / 美股代號，查看公司價值"
+          sx={{ width: "320px" }}
+        />
+      </FormControl>
+      {searchResults.length > 0 && (
+        <Stack
+          sx={{
+            position: "absolute",
+            top: "50px",
+            width: "320px",
+            border: "1px solid #dfdfdf",
+            backgroundColor: "#FFF",
+          }}
+        >
+          <Divider textAlign="left">查詢個股</Divider>
+          {searchResults.map((str, idx) => (
+            <button key={idx}>{`${str.split("-")[0]} ${
+              str.split("-")[1]
+            }`}</button>
+          ))}
+        </Stack>
+      )}
+    </Box>
+  );
 }
